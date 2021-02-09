2aa6a08a0c521821c2598300959eb64de417ef64
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2021-02-10 01:54:37 +0530

    🎉 adds Upload component 🎉 adds logo in Nav

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -1,3 +1,13 @@
-const Nav = () => <div>Nav</div>;
+import { Container, Logo } from "./styles";
+
+const Nav = () => (
+  <Container>
+    <Logo>
+      <span>Sum</span>
+      <span>Mar</span>
+      <span>Ize</span>
+    </Logo>
+  </Container>
+);
 
 export default Nav;
