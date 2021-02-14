6cb3cb33e6cfc18a4a26019d463b787d32c693c5
thealphaversion <alphabuildv1@gmail.com>
2021-02-14 19:07:10 +0530

    added hashing

--- backend/utils.py
+++ backend/utils.py
@@ -1,10 +1,19 @@
 import hashlib
 
 def hash_file(file):
-    BLOCKSIZE = 65536
-    file.seek(0)
+    # BLOCKSIZE = 65536
+    # file.seek(0)
     # hash_key = hashlib.md5()
     
-    hash_key = hashlib.md5(file.read()).hexdigest()
+    hash_val = hashlib.md5(file.read()).hexdigest()
 
-    return hash_key
+    return hash_val
+
+def build_video_object(video):
+    data = {
+        'keywords': video['keywords'],
+        'summary': video['summary'],
+        'transcript': video['transcript'],
+        'hash': video['hash']
+    }
+    return data
